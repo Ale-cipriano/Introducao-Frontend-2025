e00dd4a027a4d5a7c1611898e01e90a51ba9e272
Alessandro Cipriano.'. <136939757+Ale-cipriano@users.noreply.github.com>
2025-04-20 14:06:46 +0000

    Criação do Card 2.

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
     nome: 'Rick Sanchez',                     
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
+  
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
   return (
     <>
     <Card item={item1} />
+    <Card item={item2} />
        
     </>
   )
