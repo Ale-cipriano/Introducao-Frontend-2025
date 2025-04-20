11a41f5402ba133cd612c94d72e5fd5ef8d70842
Alessandro Cipriano.'. <136939757+Ale-cipriano@users.noreply.github.com>
2025-04-20 03:40:56 +0000

    Conteúdos HTML e CSS movidos para components 'Card'

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,7 @@ function App() {
   return (
     <>
     <Card />
-      <div className ="card">  
-        <h2>Rick Sanches</h2>
-
-        <div  className="tags">
-          <div className="tag">Status: vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137 </div>       
-        </div>
-        
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />              
-      </div>   
+       
     </>
   )
 }
