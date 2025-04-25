a43fdc7b480441823fc44627c07c7a2b99311465
Alessandro Cipriano.'. <136939757+Ale-cipriano@users.noreply.github.com>
2025-04-25 02:50:43 +0000

    Criação do componente  '<Tag />' (WIP)

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,14 @@
+import './Tag.css'
+
+export default function Tag() {
+    /*
+    Conteúdo do componente
+    */
+
+    /*
+    Return -> Encerra a função
+    no ReactJS o terturn precisa devolver um componente JSX para ser exibido na tela.
+    */
+
+    return <div>Tag funcionando!</div>
+}
