dcd87a9fa0d115a92e1bfb71c4e397b70db60137
Alessandro Cipriano.'. <136939757+Ale-cipriano@users.noreply.github.com>
2025-04-25 03:59:45 +0000

    Acrescentando elemento 'index'.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,20 @@
+
 import Tag from "../Tag/Tag";
 import "./Card.css"
 
 export default function Card(props) {
     const item = props.item;
+
+    const tags = item.tags || [];
     
     return <div className="card">
         <h2>{item.nome}</h2>
 
-        <div  className="tags">
-          <Tag />
-          {/* <div className="tag">Status: vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137 </div>  */}  
-        </div>
+        <div className="tags">
+      {tags.map(function (tag, index) {
+        return <Tag text={tag} key={`card_tag_${index}`} />
+      })}
+    </div>
         
         <img src={item.imagemUrl} />              
       </div>  
--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,14 +1,16 @@
 import './Tag.css'
 
-export default function Tag() {
+export default function Tag(props) {
     /*
     Conteúdo do componente
     */
+
+    const text = props.text;
 
     /*
     Return -> Encerra a função
     no ReactJS o terturn precisa devolver um componente JSX para ser exibido na tela.
     */
 
-    return <div>Tag funcionando!</div>
+    return <div className="tag">{text}</div>
 }
